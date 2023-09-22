a80cfcaa1deef4e8d0d42afe81c413d02780691d
James Krieger <jamesmkrieger@gmail.com>
2023-09-22 13:34:38 +0100

    first commit

---
+++ README.rst
@@ -0,0 +1,34 @@
+=======================
+Scipion Ensemble Reweighting plugin
+=======================
+
+This is an Ensemble Reweighting plugin for **scipion**
+
+
+**Install the plugin in devel mode**
+
+.. code-block::
+
+    scipion3 installp -p /home/me/scipion-em-reweighting --devel
+
+If installation fails, you can access pip options like:
+
+.. code-block::
+
+    scipion3 python -m pip ... (list, install, uninstall)
+    
+
+**Customize it**
+
+Replace icon.png with your logo and update the bibtex.py with your reference.
+
+Get rid of this content and keep the readme informative
+
+
+**Repository**
+
+To create the repository, following those guide depending the platform:
+
+- GitHub: https://docs.github.com/en/get-started/quickstart/create-a-repo
+- GitLab https://docs.gitlab.com/ee/user/project/repository/
+- Bitbucket https://support.atlassian.com/bitbucket-cloud/docs/create-a-git-repository/
---
+++ reweighting/bibtex.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# **************************************************************************
+# *
+# * Authors:     James Krieger (jmkrieger@cnb.csic.es)
+# *
+# * Centro Nacional de Biotecnologia, CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+"""
+@article{ensReweighting,
+    title={Ensemble Reweighting Using Cryo-EM Particle Images},
+    author={Tang, W.S. and Silva-Sánchez, D. and Giraldo-Barreto, J. and Carpenter, B. and Hanson, S.M. and Barnett, A.H. and Thiede, E.H. and Cossio, P.},
+    journal={J Phys Chem B},
+    year={2023},
+    volume={127},
+    number={24},
+    pages={5410--5421},
+    doi={10.1021/acs.jpcb.3c01087}
+}
+"""
