2b3de83065e3b7cc2c25076f0204515b2c1a639a
James Krieger <jamesmkrieger@gmail.com>
2023-09-27 16:12:26 +0100

    fix advanced params

--- reweighting/protocols/protocol_estimate.py
+++ reweighting/protocols/protocol_estimate.py
@@ -117,27 +117,27 @@ class ReweightingEstimateWeightsProtocol(EMProtocol):
         
         form.addParam('chains', params.IntParam, default=4,
                       label="Number of MCMC chains",
-                      level=params.LEVEL_ADVANCED,
+                      expertLevel=params.LEVEL_ADVANCED,
                       help='Number of chains for Markov Chain Monte Carlo for posterior sampling')
         
         form.addParam('iterwarmup', params.IntParam, default=200,
                       label="Number of warm up iterations",
-                      level=params.LEVEL_ADVANCED,
+                      expertLevel=params.LEVEL_ADVANCED,
                       help='Number of MCMC warm up steps')
         
         form.addParam('itersample', params.IntParam, default=2000,
                       label="Number of sample iterations",
-                      level=params.LEVEL_ADVANCED,
+                      expertLevel=params.LEVEL_ADVANCED,
                       help='Number of MCMC sample steps')
         
         form.addParam('parallelchain', params.IntParam, default=1,
                       label="Number of chains in parallel",
-                      level=params.LEVEL_ADVANCED,
+                      expertLevel=params.LEVEL_ADVANCED,
                       help='(for parallelization) number of chains in parallel for MCMC')
         
         form.addParam('threadsperchain', params.IntParam, default=1,
                       label="number of threads per chain",
-                      level=params.LEVEL_ADVANCED,
+                      expertLevel=params.LEVEL_ADVANCED,
                       help='(for parallelization) number of threads per chain for MCMC')
 
     # --------------------------- STEPS functions ------------------------------
