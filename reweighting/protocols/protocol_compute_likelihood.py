a4073ea3ae7027902b78e16d355c27c8caf1872c
James Krieger <jamesmkrieger@gmail.com>
2025-03-27 14:34:31 +0100

    fix class order

--- reweighting/protocols/protocol_compute_likelihood.py
+++ reweighting/protocols/protocol_compute_likelihood.py
@@ -292,30 +292,20 @@ class ReweightingProtComputeLikelihood(ProtAnalysis3D):
 
         clsDict = {}  # Dictionary to store the (classId, classSet) pairs
 
+        for ref, rep in refsDict.items():
+            # add empty classes
+            classItem = clsSet.ITEM_TYPE.create(self._getExtraPath(), suffix=ref+1)
+            classItem.setRepresentative(rep)
+            clsDict[ref] = classItem
+            clsSet.append(classItem)
+
         cls_prev = 1
-        rep = refsDict[cls_prev]
         for img, ref in izip(inputParticles, classIds):
             if ref != cls_prev:
                 cls_prev = ref
-                rep = refsDict[cls_prev]
 
-            if ref not in clsDict:
-                classItem = clsSet.ITEM_TYPE.create(self._getExtraPath(), suffix=ref+1)
-                classItem.setRepresentative(rep)
-                clsDict[ref] = classItem
-                clsSet.append(classItem)
-            else:
-                classItem = clsDict[ref]
-
+            classItem = clsDict[ref]
             classItem.append(img)
-
-        for ref, rep in refsDict.items():
-            if rep.getFileName() not in [class3d.getRepresentative().getFileName()
-                              for class3d in clsDict.values()]:
-                classItem = clsSet.ITEM_TYPE.create(self._getExtraPath(), suffix=ref+1)
-                classItem.setRepresentative(rep)
-                clsDict[ref] = classItem
-                clsSet.append(classItem)
 
         for classItem in clsDict.values():
             clsSet.update(classItem)
