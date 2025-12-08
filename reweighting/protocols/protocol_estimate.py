ae17d770af84c9290f824c23d87ef63726263869
James Krieger <jamesmkrieger@gmail.com>
2025-12-08 15:20:26 +0000

    rm traj objs

--- reweighting/protocols/protocol_estimate.py
+++ reweighting/protocols/protocol_estimate.py
@@ -74,7 +74,7 @@ class ReweightingEstimateWeightsProtocol(EMProtocol):
         form.addParam('clusterSizePointer', params.PointerParam, 
                       label="Cluster size object",
                       condition='infileClusterSizeData == USE_POINTER',
-                      pointerClass='SetOfAtomStructs,SetOfTrajFrames,SetOfVolumes,SetOfClassesTraj',
+                      pointerClass='SetOfAtomStructs,SetOfVolumes',
                       help='The input structures can be any set that contains sizes')
         
         form.addParam('infileImageDistanceData', params.EnumParam, choices=['file', 'pointer'],
